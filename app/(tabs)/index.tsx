5e5b9242cb355bab2129e322c578df3ae1af799f
Kunal <kunalgupta0550@gmail.com>
2025-09-08 00:00:46 +0530

    create tabs

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,7 +5,6 @@ export default function Index() {
   return (
     <View className="flex-1 items-center justify-center">
       <Text className="text-5xl text-dark-200">Welcome!</Text>
-      <Link href="/onboarding" >Onboarding</Link>
     </View>
   );
 }
